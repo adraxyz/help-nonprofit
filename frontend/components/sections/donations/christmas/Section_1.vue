d75b267c7fdc44562ffac1ebcdee8f28653901d1
adraxyz <adrax@autistici.org>
2020-11-18 12:39:10 +0100

    fix columns order

--- frontend/components/sections/donations/christmas/Section_1.vue
+++ frontend/components/sections/donations/christmas/Section_1.vue
@@ -3,7 +3,7 @@
 
     <v-row no-gutters class="donation-row padding-level-3" align="center">
 
-      <v-col cols="12" sm="4" class="donation-col"
+      <v-col cols="12" sm="4" class="donation-col" :order="p.order"
              v-for="p in products" :key="p.order">
         <ShopItemCard :product="p" :texts="texts"/>
       </v-col>
